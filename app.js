20925e1c29d9814908752bdec82e59e17964dfea
Zahid Mahir <zahid@zmhr.me>
2014-10-02 15:54:07 -0400

    clean up

--- app.js
+++ app.js
@@ -3,29 +3,37 @@
 /* Application */
 
 var wife = angular.module('Wife', [])
-    .controller('expenseController', function($scope, expenseService) {
-        $scope.expenses = {
-            list : [],
-            new_expense : { name : null, cost : null, description: null },  
-            add_expense : function() {
-                if($scope.expenses.new_expense.name && $scope.expenses.new_expense.cost) {
-                    $scope.expenses.new_expense.date = new Date();
-                    $scope.expenses.list.push($scope.expenses.new_expense);
-                    $scope.expenses.new_expense = { name : null, cost : null, description: null };
-                }
-            }
-        };
-        expenseService.getExpenses().then(function (data) {
-            console.log('got expenses', data);
-            $scope.expenses.list = data.items;
-        });
-    })
-    .service('expenseService', function($http) {
-        this.getExpenses = function() {
-            return $http.get('/dummy.json')
-                .then(function(response) {
-                    return response.data;
-                });
+.controller('expenseController', function($scope, expenseService) {
+    $scope.expenses = {
+        list : [],
+        new_expense : { name : null, cost : null, description: null },  
+        add_expense : function() {
+            $scope.expenses.new_expense.date = new Date();
+            $scope.expenses.list.push($scope.expenses.new_expense);
+            $scope.expenses.new_expense = { name : null, cost : null, description: null };
+        },
+        totals : {
+            cost : 0,
+            items : 0
         }
+    };
+    $scope.$watch('expenses.list', function(o,n) {
+        var total_cost = 0;
+        for(var item in $scope.expenses.list) {
+            total_cost += $scope.expenses.list[item].cost;
+        }
+        $scope.expenses.totals.cost = total_cost
+        $scope.expenses.totals.items = $scope.expenses.list.length;
     });
-
+    expenseService.load_expenses().then(function (data) {
+        $scope.expenses.list = data.items;
+    });
+})
+.service('expenseService', function($http) {
+    this.load_expenses = function() {
+        return $http.get('/dummy.json')
+            .then(function(response) {
+                return response.data;
+            });
+    }
+});
